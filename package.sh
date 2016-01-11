a125523a26da9367f6036b24425fbad2753ce5d2
jay@aurora <jay@aurora>
2016-01-11 15:31:06 -0500

    Change package script to add license and readme

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 cp out/artifacts/TaflEngine_jar/OpenTafl.jar package/
 cd package
-zip OpenTafl.zip OpenTafl.* OpenTafl*bat ansicon-x86/*.* ansicon-x64/*.*
+zip OpenTafl.zip OpenTafl.* OpenTafl*bat ansicon-x86/*.* ansicon-x64/*.* README.txt LICENSE.txt
 rm OpenTafl.jar
 cd ..
 mv package/OpenTafl.zip .
