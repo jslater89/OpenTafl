d2559eeb823818ac2892a00b5492b81a003a7301
jay@aurora <jay@aurora>
2016-03-14 12:38:33 -0400

    Packaging for engine config file
    --HG--
    branch : v0.2.x

--- package.sh
+++ package.sh
@@ -1,8 +1,9 @@
 cp out/artifacts/TaflEngine_jar/OpenTafl.jar package/
 cp README.txt package/
 cp LICENSE.txt package/
+cp engines/opentafl.ini package/engines
 cd package
-zip OpenTafl.zip OpenTafl.* OpenTafl*bat README.txt LICENSE.txt
+zip OpenTafl.zip OpenTafl.* OpenTafl*bat engines/* README.txt LICENSE.txt
 rm OpenTafl.jar
 cd ..
 mv package/OpenTafl.zip .
