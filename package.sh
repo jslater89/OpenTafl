3faa4c10c62d2d3f7dcb6e070eb432be3514120e
jay@aurora <jay@aurora>
2016-04-02 14:26:41 -0400

    Improve package system
    --HG--
    branch : v0.2.x

--- package.sh
+++ package.sh
@@ -1,9 +1,17 @@
+mkdir package/engines
+mkdir package/saved-games
+mkdir package/saved-games/replays
+
+cp unbuilt/* package
 cp out/artifacts/TaflEngine_jar/OpenTafl.jar package/
 cp README.txt package/
 cp LICENSE.txt package/
 cp engines/opentafl.ini package/engines
+cp saved-games/replays/* package/saved-games/replays
 cd package
-zip OpenTafl.zip OpenTafl.* OpenTafl*bat engines/* README.txt LICENSE.txt
+zip OpenTafl.zip OpenTafl.* OpenTafl*bat engines/* saved-games/* saved-games/replays/* README.txt LICENSE.txt
 rm OpenTafl.jar
 cd ..
 mv package/OpenTafl.zip .
+
+rm -rf package/*
