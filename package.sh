a7a180b21bbd7139978ef03be90d70a5992a1904
jay@aurora <jay@aurora>
2016-04-02 15:23:23 -0400

    Package script updates
    --HG--
    branch : v0.2.x

--- package.sh
+++ package.sh
@@ -1,17 +1,16 @@
+mkdir package
 mkdir package/engines
 mkdir package/saved-games
 mkdir package/saved-games/replays
 
-cp unbuilt/* package
+cp -R unbuilt/* package
 cp out/artifacts/TaflEngine_jar/OpenTafl.jar package/
 cp README.txt package/
 cp LICENSE.txt package/
-cp unbuilt/engines/opentafl.ini package/engines
-cp unbuilt/saved-games/replays/* package/saved-games/replays
 cd package
 zip OpenTafl.zip OpenTafl.* OpenTafl*bat engines/* saved-games/* saved-games/replays/* README.txt LICENSE.txt
 rm OpenTafl.jar
 cd ..
 mv package/OpenTafl.zip .
 
-rm -rf package/*
+rm -rf package/
