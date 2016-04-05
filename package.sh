1f6c2a335fd271aa549b0287ef97bd9566bcd57b
jay@aurora <jay@aurora>
2016-04-05 13:11:27 -0400

    Update license, fix external rules
    --HG--
    branch : v0.2.x

--- package.sh
+++ package.sh
@@ -8,8 +8,7 @@ cp out/artifacts/TaflEngine_jar/OpenTafl.jar package/
 cp README.txt package/
 cp LICENSE.txt package/
 cd package
-zip OpenTafl.zip OpenTafl.* OpenTafl*bat engines/* saved-games/* saved-games/replays/* README.txt LICENSE.txt
-rm OpenTafl.jar
+zip OpenTafl.zip OpenTafl.* OpenTafl*bat external-rules.conf engines/* saved-games/* saved-games/replays/* README.txt LICENSE.txt
 cd ..
 mv package/OpenTafl.zip .
 
