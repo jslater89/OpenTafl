7feb0ebd1bf596856c5e2a8f760cedfb4597b579
jay@aurora <jay@aurora>
2016-05-25 15:26:58 -0400

    Improve XorshiftRandom from xorshift64 to xorshift128+
    --HG--
    branch : v0.3.x

--- src/com/manywords/softworks/tafl/engine/XorshiftRandom.java
+++ src/com/manywords/softworks/tafl/engine/XorshiftRandom.java
@@ -6,21 +6,24 @@ import java.util.Random;
  * Created by jay on 12/28/15.
  */
 public class XorshiftRandom extends Random {
-    private long seed;
+    private long seed1;
+    private long seed2;
 
-    public XorshiftRandom() {
-        this.seed = System.currentTimeMillis();
-    }
+    public XorshiftRandom() { this(System.currentTimeMillis()); }
     public XorshiftRandom(long seed) {
-        this.seed = seed;
+        this.seed1 = seed;
+        this.seed2 = ~seed;
     }
 
     protected synchronized int next(int nbits) {
-        long x = this.seed;
-        x ^= (x << 21);
-        x ^= (x >>> 35);
-        x ^= (x << 4);
-        this.seed = x;
+        long x = this.seed1;
+        long y = this.seed2;
+        this.seed1 = y;
+
+        x ^= (x << 23); // a
+        this.seed2 = x ^ y ^ (x >>> 17) & (y >>> 26); //b, c
+        x = seed2 + y;
+
         x &= ((1L << nbits) -1);
         return (int) x;
     }
