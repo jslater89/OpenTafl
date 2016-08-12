2273eec788c28e86e457cc25377e7f1fce7366f7
jay@sif <jay@sif>
2016-08-12 10:50:49 -0400

    Fix speed limit test to match modified Magpie rules
    --HG--
    branch : v0.4.x-unstable

--- src/com/manywords/softworks/tafl/test/rules/SpeedLimitTest.java
+++ src/com/manywords/softworks/tafl/test/rules/SpeedLimitTest.java
@@ -2,10 +2,13 @@ package com.manywords.softworks.tafl.test.rules;
 
 import com.manywords.softworks.tafl.engine.Game;
 import com.manywords.softworks.tafl.engine.GameState;
+import com.manywords.softworks.tafl.engine.MoveRecord;
+import com.manywords.softworks.tafl.rules.Coord;
 import com.manywords.softworks.tafl.rules.Rules;
 import com.manywords.softworks.tafl.rules.Taflman;
 import com.manywords.softworks.tafl.rules.brandub.Magpie;
 import com.manywords.softworks.tafl.test.TaflTest;
+import com.manywords.softworks.tafl.ui.RawTerminal;
 
 /**
  * Created by jay on 8/10/16.
@@ -18,6 +21,20 @@ public class SpeedLimitTest extends TaflTest {
 
         GameState s = g.getCurrentState();
         char taflman = s.getPieceAt(0, 3);
-        assert Taflman.getAllowableDestinations(s, taflman).size() == 2;
+
+        assert Taflman.getAllowableDestinations(s, taflman).size() == 4;
+
+        s = g.getCurrentState();
+        s.setCurrentSide(s.getDefenders());
+        s.makeMove(new MoveRecord(Coord.get(2,3), Coord.get(2, 0)));
+
+        s = g.getCurrentState();
+        s.setCurrentSide(s.getDefenders());
+        s.makeMove(new MoveRecord(Coord.get(3,3), Coord.get(2, 3)));
+
+        s = g.getCurrentState();
+        taflman = s.getPieceAt(2,3);
+        assert Taflman.isKing(taflman);
+        assert Taflman.getAllowableDestinations(s, taflman).size() == 3;
     }
 }
