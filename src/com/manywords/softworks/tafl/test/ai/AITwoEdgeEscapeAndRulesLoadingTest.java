3483046eb557c1fa5dfa8d1859cdd15baf956c2a
jay@sif <jay@sif>
2016-12-14 18:51:24 -0500

    Fix two-edge escape test
    --HG--
    branch : better-eval-only

--- src/com/manywords/softworks/tafl/test/ai/AITwoEdgeEscapeAndRulesLoadingTest.java
+++ src/com/manywords/softworks/tafl/test/ai/AITwoEdgeEscapeAndRulesLoadingTest.java
@@ -1,5 +1,6 @@
 package com.manywords.softworks.tafl.test.ai;
 
+import com.manywords.softworks.tafl.OpenTafl;
 import com.manywords.softworks.tafl.engine.Game;
 import com.manywords.softworks.tafl.engine.GameState;
 import com.manywords.softworks.tafl.engine.MoveRecord;
@@ -12,6 +13,10 @@ import com.manywords.softworks.tafl.test.TaflTest;
 
 public class AITwoEdgeEscapeAndRulesLoadingTest extends TaflTest {
 
+    @Override
+    public void statusText(String text) {
+        OpenTafl.logPrintln(OpenTafl.LogLevel.CHATTY, text);
+    }
 
     @Override
     public void run() {
@@ -34,37 +39,30 @@ public class AITwoEdgeEscapeAndRulesLoadingTest extends TaflTest {
         int victory = GameState.GOOD_MOVE;
         int value = 0;
 
-        for(int i = 0; i < 2; i++) {
-            //RawTerminal.renderGameState(state);
-            AiWorkspace workspace = new AiWorkspace(this, game, state, 5);
-            //workspace.chatty = true;
-            workspace.explore(5);
-            MoveRecord nextMove = workspace.getTreeRoot().getBestChild().getEnteringMove();
-            value = workspace.getTreeRoot().getBestChild().getValue();
 
-            /*
-            System.out.println("value: " + value);
-            for(GameTreeNode node : workspace.getTreeRoot().getBestPath()) {
-                for(GameTreeNode child : node.getBranches()) {
-                    System.out.print(child.getEnteringMove() + ", ");
-                }
-                System.out.println();
-                System.out.println("Node: " + node.getEnteringMove() + " " + value);
+        //RawTerminal.renderGameState(state);
+        AiWorkspace workspace = new AiWorkspace(this, game, state, 5);
+        workspace.chatty = true;
+        workspace.explore(5);
+        MoveRecord nextMove = workspace.getTreeRoot().getBestChild().getEnteringMove();
+        value = workspace.getTreeRoot().getBestChild().getValue();
+        state.makeMove(nextMove);
+
+        /*
+        System.out.println("value: " + value);
+        for(GameTreeNode node : workspace.getTreeRoot().getBestPath()) {
+            for(GameTreeNode child : node.getBranches()) {
+                System.out.print(child.getEnteringMove() + ", ");
             }
-            */
-
-            workspace.printSearchStats();
-
-            victory = state.checkVictory();
-            if(victory != GameState.GOOD_MOVE) {
-                break;
-            }
-            else {
-                state.makeMove(nextMove);
-            }
-
-            state = game.getCurrentState();
+            System.out.println();
+            System.out.println("Node: " + node.getEnteringMove() + " " + value);
         }
+        */
+
+        workspace.printSearchStats();
+
+
+        state = game.getCurrentState();
 
         victory = state.checkVictory();
         //RawTerminal.renderGameState(state);
